a25adb14b794d99235d3c3ad1b0aaef98a5d2890
Mansi Mehra <mansi2004mehra18@gmail.com>
2025-05-24 17:58:38 +0530

    additions

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -6,7 +6,7 @@ const Orders = () => {
   const [allOrders, setAllOrders] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allOrders").then((res) => {
+    axios.get("https://z3-uv3w.onrender.com/allOrders").then((res) => {
       console.log(res.data);
       setAllOrders(res.data);
     });
